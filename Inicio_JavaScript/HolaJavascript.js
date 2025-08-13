f375de75c68b86b0ed183b639243a2d1a28267ce
Luca <gmerinoluca@gmail.com>
2025-08-13 13:55:41 +0200

    tipos de datos

--- Inicio_JavaScript/HolaJavascript.js
+++ Inicio_JavaScript/HolaJavascript.js
@@ -1,1 +1,26 @@
 console.log("Hola chavalada");
+
+//variables y constantes//
+var y=1;
+let x=2;
+const z=3;
+
+//declarar variable//
+
+let a;
+
+//inicializar variable//
+//1-Declarar e inicializar a la vez//
+let b=1;
+//2-Una variable ya declarada se le da un valor//
+a=1;
+
+//Tipos de datos//
+let entero=1;
+let decimal=2.67;
+let texto="Hola" //dato de tipo string, o cadena de caracteres//
+let textonum="76";
+let booleano=true; //ó false//
+
+
+console.log(a);
